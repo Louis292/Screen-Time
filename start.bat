bd9596e9cc79e92c4657c943db687b12ad724310
Louis292 <105144467+Louis292@users.noreply.github.com>
2025-01-17 20:42:31 +0100

    create start.bat

--- start.bat
+++ start.bat
@@ -1,1 +1,13 @@
+@echo off
+:: Vérifie si pip est installé
+python -m ensurepip --default-pip >nul 2>&1
+
+:: Installe les dépendances si elles ne sont pas présentes
+pip install -r requirements.txt || (
+    echo Une erreur s'est produite lors de l'installation des dépendances.
+    pause
+    exit /b
+)
+
+:: Lance le script main.py
 python main.py
